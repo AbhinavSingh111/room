044c5cdd43cc730d917976b3a266602e7794d06b
Abhinav Singh <singh111abhinav@gmail.com>
2024-04-02 20:25:07 +0530

    changes to fix timezone issue

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,15 +2,20 @@ import MeetingTypeList from '@/components/ui/MeetingTypeList';
 import React from 'react'
 
 const home = () => {
+  // const now = new Date();
+  // const time = now.toLocaleTimeString('en-US', {hour:'2-digit', minute:'2-digit'})
+  // const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
+  // to get loacl time when deploying on vercel
   const now = new Date();
-  const time = now.toLocaleTimeString('en-US', {hour:'2-digit', minute:'2-digit'})
-  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
+  const options = { timeZone: 'Asia/Kolkata', hour: '2-digit', minute: '2-digit' };
+  const time = now.toLocaleTimeString('en-IN', options);
+  const date = new Intl.DateTimeFormat('en-IN', { dateStyle: 'full', timeZone: 'Asia/Kolkata' }).format(now);
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
           <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>
-            Upcoming meeting at 12:30
+            Hi ! Hope you had your water.
           </h2>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-extrabold lg:text-7xl'>
